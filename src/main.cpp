8692ec00d027e39f5d7cc560f27c618f1df7baba
Kanstantinas Piatrashka <kanstantinasp@gmail.com>
2020-06-23 19:22:45 +0300

    Make function clearConsole inline

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 
-void clearConsole()
+inline void clearConsole()
 {
     // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
     std::cout << "\x1B[2J\x1B[H";
