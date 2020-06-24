0363a41bfe849a6d7c25e75c5266ec049be59cc0
Kanstantinas Piatrashka <kanstantinasp@gmail.com>
2020-06-24 09:53:23 +0300

    Create constructor and change some details of Deck class header

--- src/card/card.h
+++ src/card/card.h
@@ -5,15 +5,20 @@
 /*
  * Card represents a card from 2 to A.
  * Operations:
+ *  Create a card
  * 	Print to stdout
  * 	Get value of a card (11 for Ace)
  */
 class Card {
 	public:
+    Card(const char* card); // Create card from C-style string.
+                            // If string representation of card is not valid,
+                            // constructor will throw runtime error
 		friend std::ostream& operator<<(std::ostream& os, const Card& card); // Prints card to stdout
-		unsigned getValue(); // Returns value of a card. A is treated as 11
+		inline short getValue(); // Returns value of a card. A is treated as 11
 	private:
-		char value[2]; // value[0] is number or a letter
-			       // value[1] is a card suit
+		char value[3]; // value[0] is number or a letter
+			             // value[1] is a card suit
+                   // value[2] is '\0'
 };
 #endif
