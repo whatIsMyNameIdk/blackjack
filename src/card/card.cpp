46af718a58b9960fd48f7b54665986b494a1a769
Kanstantinas Piatrashka <kanstantinasp@gmail.com>
2020-06-26 10:46:10 +0300

    Make Card member functions

--- src/card/card.cpp
+++ src/card/card.cpp
@@ -30,3 +30,40 @@ Card::Card(const std::string &card)
       throw (std::runtime_error("Invalid card number or letter"));   
   }
 }
+
+std::ostream& operator<<(std::ostream& os, const Card& card)
+{
+  if (card.value == '1')
+    return os << 10;
+  else
+    return os << card.value;
+}
+
+inline short Card::getValue()
+{
+  switch (value) {
+    case '2':
+      return 2;
+    case '3':
+      return 3;
+    case '4':
+      return 4;
+    case '5':
+      return 5;
+    case '6':
+      return 6;
+    case '7':
+      return 7;
+    case '8':
+      return 8;
+    case '9':
+      return 9;
+    case '1':
+    case 'J':
+    case 'Q':
+    case 'K':
+      return 10;
+    case 'A':
+      return 11;
+  }
+}
